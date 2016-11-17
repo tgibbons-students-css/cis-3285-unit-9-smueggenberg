4e97ff5eee364a4ae419c24944b768dee0205799
smueggenberg <smueggenberg@css.edu>
2016-11-17 13:19:26 -0600

    Added logging functionality

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 using SingleResponsibilityPrinciple.Contracts;
+using System.IO;
 
 namespace SingleResponsibilityPrinciple
 {
@@ -8,11 +9,29 @@ namespace SingleResponsibilityPrinciple
     {
         public void LogWarning(string message, params object[] args)
         {
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log>\n" +
+                                  "  <type>WARN</type>\n" +
+                                  "  <message>" + message + "</message>\n" +
+                                  "</log>"
+                                  , args);
+            }
+
             Console.WriteLine(string.Concat("WARN: ", message), args);
         }
 
         public void LogInfo(string message, params object[] args)
         {
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log>\n" +
+                                  "  <type>INFO</type>\n" +
+                                  "  <message>" + message + "</message>\n" +
+                                  "</log>"
+                                  , args);
+            }
+
             Console.WriteLine(string.Concat("INFO: ", message), args);
         }
     }
